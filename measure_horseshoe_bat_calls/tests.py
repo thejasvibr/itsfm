72d09d5913e47369bbf3ea18c0c7dffca24f156b
thejasvibr <tbeleyur@orn.mpg.de>
2020-02-14 14:36:44 +0100

    change code repo name and remove mentions of CF

---
+++ measure_horseshoe_bat_calls/tests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""tests for measure_single_horseshoe_bat_call
+Created on Wed Feb 12 17:53:58 2020
+
+@author: tbeleyur
+"""
+from measure_single_horseshoe_bat_call import *
+
+import unittest
+
+
+
+
+class TestingAttachAnnotation(unittest.TestCase):
+    
+    def setUp(self):
+      
+        
+
+    def test_basic(self):
+        
+        
